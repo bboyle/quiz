e95baeb1a0fd8b687a33513700af09b817b6a077
Ben Boyle <benjamins.boyle@gmail.com>
2014-10-20 13:13:15 +1000

    remove unused temporary variables
    When you find yourself naming variables like this:
    _question1, question2, question3, ..._
    It indicates you need an array (which you have as well).
    
    This change just crushes the quiz data down into one variable.
    It's worth getting used to this format, it's very similar to JSON.
    (JSON is strict about syntax, e.g. must use double quotes on everything).

--- js/quizzData.js
+++ js/quizzData.js
@@ -1,49 +1,45 @@
-var questionData1 = {
-	QuestionType: 0,
-	Question:"What is Question 1?",
-	Answers: ["1", "2", "3", "4" ],
-	AnswerCount:4,
-	CorrectAnswer:1,
-	UserAnswered:0
-};
-
-var questionData2 = {
-	QuestionType: 0,
-	Question:"What is Question 2?",
-	Answers: ["1", "2", "3", "4" ],
-	AnswerCount:4,
-	CorrectAnswer:2,
-	UserAnswered:0
-};
-
-var questionData3 = {
-	QuestionType: 0,
-	Question:"What is Question 3?",
-	Answers: ["1", "2", "3", "4" ],
-	AnswerCount:4,
-	CorrectAnswer:3,
-	UserAnswered:0
-};
-
-var questionData4 = {
-	QuestionType: 0,
-	Question:"What is Question 4?",
-	Answers: ["1", "2", "3", "4" ],
-	AnswerCount:4,
-	CorrectAnswer:3,
-	UserAnswered:0
-};
-
-var questionData5 = {
-	QuestionType: 0,
-	Question:"What is Question 5?",
-	Answers: ["1", "2", "3", "4" ],
-	AnswerCount:4,
-	CorrectAnswer:1,
-	UserAnswered:0
-};
-
-var quizzQuestions = [questionData1, questionData2, questionData3, questionData4, questionData5];
+var quizzQuestions = [
+	{
+		QuestionType: 0,
+		Question:"What is Question 1?",
+		Answers: ["1", "2", "3", "4" ],
+		AnswerCount:4,
+		CorrectAnswer:1,
+		UserAnswered:0
+	},
+	{
+		QuestionType: 0,
+		Question:"What is Question 2?",
+		Answers: ["1", "2", "3", "4" ],
+		AnswerCount:4,
+		CorrectAnswer:2,
+		UserAnswered:0
+	},
+	{
+		QuestionType: 0,
+		Question:"What is Question 3?",
+		Answers: ["1", "2", "3", "4" ],
+		AnswerCount:4,
+		CorrectAnswer:3,
+		UserAnswered:0
+	},
+	{
+		QuestionType: 0,
+		Question:"What is Question 4?",
+		Answers: ["1", "2", "3", "4" ],
+		AnswerCount:4,
+		CorrectAnswer:3,
+		UserAnswered:0
+	},
+	{
+		QuestionType: 0,
+		Question:"What is Question 5?",
+		Answers: ["1", "2", "3", "4" ],
+		AnswerCount:4,
+		CorrectAnswer:1,
+		UserAnswered:0
+	}
+];
 
 function getQuizz()
 {
